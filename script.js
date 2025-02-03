a2d60f3030b982c93290ab4d88d3b5b25026f02a
Albina <alisaKovaleva993@.com>
2025-02-03 17:36:28 +0300

    site

---
+++ script.js
@@ -0,0 +1,47 @@
+alert("hello world") // vlad venom
+var a = 5;
+var b = 5; 
+var res = a+b;
+console.log(res); //от себя
+
+// 1 var
+
+console.log(a);
+var a = 5;
+console.log(a); // выведет 5
+
+if(true) {
+    var b = 10;
+}
+console.log(b);
+
+let x = 5;
+console.log("x = " + x);  // выведет 5
+
+if (true) {
+    let y = 10;
+    console.log("y = " + y) 
+}
+
+// 3 const
+
+const PI = 3.14
+console.log("PI = "+ PI) // число PI
+
+const arr = [1,2,3];
+arr.push(4) // добавляем в массив число 4
+console.log(arr)
+
+console.log(arr[1])
+
+
+//возводим степ
+let o = 10;
+alert(o ** 3) // выведет 1000
+
+let str = "abc";
+console.log(str)
+
+
+let str1 = "dev" + "ops";
+console.log(str1);
